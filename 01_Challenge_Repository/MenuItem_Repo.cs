0cb8ba6f6972189189dc871603dc095e33e920ac
Alexandro Cazares <CazaresCode@gmail.com>
2021-05-16 21:36:42 -0400

    first commit

---
+++ 01_Challenge_Repository/MenuItem_Repo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldBadgeConsoleApplicationChallenges.KomodoCafe_Repository
+{
+    public class MenuItem_Repository
+    {
+        private List<MenuItem> _listOfMenuItems = new List<MenuItem>();
+
+        // Create
+        public void AddMenuToList(MenuItem item)
+        {
+            _listOfMenuItems.Add(item);
+        }
+
+        // Read
+        public List<MenuItem> GetMenuItems()
+        {
+            return _listOfMenuItems;
+        }
+
+        // Update
+        public bool UpdateExisitingMenuItemByName(string menuItemName, MenuItem updatedMenuItemName)
+        {
+            MenuItem oldItem = GetMenuItemByName(menuItemName);
+
+            if (oldItem != null)
+            {
+                oldItem.MealNumber = updatedMenuItemName.MealNumber;
+                oldItem.MealName = updatedMenuItemName.MealName;
+                oldItem.Description = updatedMenuItemName.Description;
+                oldItem.Ingredients = updatedMenuItemName.Ingredients;
+                oldItem.Price = updatedMenuItemName.Price;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        // Delete
+        public bool RemoveMenuItemFromList(string menuItemName)
+        {
+            MenuItem item = GetMenuItemByName(menuItemName);
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            int intialCount = _listOfMenuItems.Count;
+            _listOfMenuItems.Remove(item);
+
+            if (intialCount > _listOfMenuItems.Count)
+            {
+                return true;
+                //could add a cw successfully removed item...
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Helper Methods
+
+        private MenuItem GetMenuItemByName(string menuItemName)
+        {
+            foreach (MenuItem item in _listOfMenuItems)
+            {
+                if (item.MealName == menuItemName)
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ 01_Challenge_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("01_Challenge_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("01_Challenge_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a71ead39-5aa7-4ec6-8327-5c103108a6d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
